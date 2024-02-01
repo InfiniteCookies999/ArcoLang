bd927cca6895e4973ea0aa13f3abcb7b18c9a366
InfiniteCookies999 <infinitecookies959@gmail.com>
2024-01-31 22:00:01 -0500

    Forgot to make checks

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -70,66 +70,66 @@ void RunTest(const char* TestSource, const std::string& ExpectedOutput) {
 
 int main() {
 
-	//RunTest(SRC("hello_world.arco"), "Hello World!");
-	//RunTest(SRC("binary_operators.arco"), "464");
-	//RunTest(SRC("hex_literals.arco"), "43809 1048575 0 11259375");
-	//RunTest(SRC("bin_literals.arco"), "941 32767 0 1404243");
-	//RunTest(SRC("increments.arco"), "2 0 0 1 1 0 1 0");
-	//RunTest(SRC("pointer_arithmetic.arco"), "hello worldello worldo worldworld worldello world");
-	//RunTest(SRC("heap_alloc.arco"), "315 341 88 341 66 341 66 341 66 341 66");
-	//RunTest(SRC("bool_literals.arco"), "ok1ok2ok3ok4");
-	//
-	//RunTest(SRC("loops/loops1.arco"), "10");
-	//RunTest(SRC("loops/loops2.arco"), "10");
-	//RunTest(SRC("loops/loops3.arco"), "10");
-	//RunTest(SRC("loops/loops4.arco"), "1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41 43 45 47 49 ");
-	//
-	//RunTest(SRC("arrays/arrays1.arco"), "21 55 11 56 3 ");
-	//RunTest(SRC("arrays/arrays2.arco"), "412 21 5 6 4 0 ");
-	//RunTest(SRC("arrays/arrays3.arco"), "412 77 121 45 56 ");
-	//RunTest(SRC("arrays/arrays4.arco"), "214 452 12 32 11 ");
-	//RunTest(SRC("arrays/arrays5.arco"), "253 23 0 13 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ");
-	//RunTest(SRC("arrays/arrays6.arco"), "5 5 5 5 5 5 5 5 ");
-	//RunTest(SRC("arrays/arrays7.arco"), "1 2 3 4 5 ");
-	//RunTest(SRC("arrays/arrays8.arco"), "");
-	//RunTest(SRC("arrays/arrays9.arco"), "24 1 523 0 0 ");
-	//RunTest(SRC("arrays/arrays10.arco"), "21 55 11 56 3 33 5 1 0 8 3 72 22 ");
-	//RunTest(SRC("arrays/arrays11.arco"), "14 99 55 1 121 ");
-	//RunTest(SRC("arrays/arrays12.arco"), "pizza is good");
-	//RunTest(SRC("arrays/arrays13.arco"), "25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 ");
-	//RunTest(SRC("arrays/arrays14.arco"), "1 2 3 4 5 6 7 8 9 10 11 99 ");
-	//RunTest(SRC("arrays/decay_testing.arco"), "9 14 235 11 5 77 22 76 11 9");
-	//
-	//RunTest(SRC("structs/structs1.arco"), "55 44");
-	//RunTest(SRC("structs/structs2.arco"), "55 44");
-	//RunTest(SRC("structs/structs3.arco"), "55 44");
-	//RunTest(SRC("structs/structs4.arco"), "55 44");
-	//RunTest(SRC("structs/structs5.arco"), "124 66 25");
-	//RunTest(SRC("structs/structs6.arco"), "124 66 25");
-	//RunTest(SRC("structs/structs7.arco"), "55 99");
-	//RunTest(SRC("structs/structs8.arco"), "55 55 55");
-	//
-	//RunTest(SRC("returns/returns1.arco"), "55 55");
-	//RunTest(SRC("returns/returns2.arco"), "55 55");
-	//RunTest(SRC("returns/returns3.arco"), "55 55");
-	//RunTest(SRC("returns/returns4.arco"), "55 55");
-	//RunTest(SRC("returns/returns5.arco"), "55 55");
-	//RunTest(SRC("returns/returns6.arco"), "55 55");
-	//
-	//RunTest(SRC("member_functions/member_functions1.arco"), "1234");
-	//RunTest(SRC("member_functions/member_functions2.arco"), "1234");
-	//RunTest(SRC("member_functions/member_functions3.arco"), "foo1foo2");
-	//RunTest(SRC("member_functions/member_functions4.arco"), "22");
-	//RunTest(SRC("member_functions/constructors1.arco"), "53");
-	//RunTest(SRC("member_functions/constructors2.arco"), "55");
-	//
-	//RunTest(SRC("globals/globals1.arco"), "44");
-	//RunTest(SRC("globals/globals2.arco"), "32 186");
-	//RunTest(SRC("globals/globals3.arco"), "34 66");
-	//RunTest(SRC("globals/globals4.arco"), "22 1111");
-	//RunTest(SRC("globals/globals5.arco"), "235 31 235 31 235 31 235 31 235 31 ");
+	RunTest(SRC("hello_world.arco"), "Hello World!");
+	RunTest(SRC("binary_operators.arco"), "464");
+	RunTest(SRC("hex_literals.arco"), "43809 1048575 0 11259375");
+	RunTest(SRC("bin_literals.arco"), "941 32767 0 1404243");
+	RunTest(SRC("increments.arco"), "2 0 0 1 1 0 1 0");
+	RunTest(SRC("pointer_arithmetic.arco"), "hello worldello worldo worldworld worldello world");
+	RunTest(SRC("heap_alloc.arco"), "315 341 88 341 66 341 66 341 66 341 66");
+	RunTest(SRC("bool_literals.arco"), "ok1ok2ok3ok4");
 	
-	RunTest(SRC("workpad"), "");
+	RunTest(SRC("loops/loops1.arco"), "10");
+	RunTest(SRC("loops/loops2.arco"), "10");
+	RunTest(SRC("loops/loops3.arco"), "10");
+	RunTest(SRC("loops/loops4.arco"), "1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41 43 45 47 49 ");
+	
+	RunTest(SRC("arrays/arrays1.arco"), "21 55 11 56 3 ");
+	RunTest(SRC("arrays/arrays2.arco"), "412 21 5 6 4 0 ");
+	RunTest(SRC("arrays/arrays3.arco"), "412 77 121 45 56 ");
+	RunTest(SRC("arrays/arrays4.arco"), "214 452 12 32 11 ");
+	RunTest(SRC("arrays/arrays5.arco"), "253 23 0 13 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ");
+	RunTest(SRC("arrays/arrays6.arco"), "5 5 5 5 5 5 5 5 ");
+	RunTest(SRC("arrays/arrays7.arco"), "1 2 3 4 5 ");
+	RunTest(SRC("arrays/arrays8.arco"), "");
+	RunTest(SRC("arrays/arrays9.arco"), "24 1 523 0 0 ");
+	RunTest(SRC("arrays/arrays10.arco"), "21 55 11 56 3 33 5 1 0 8 3 72 22 ");
+	RunTest(SRC("arrays/arrays11.arco"), "14 99 55 1 121 ");
+	RunTest(SRC("arrays/arrays12.arco"), "pizza is good");
+	RunTest(SRC("arrays/arrays13.arco"), "25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 25 55 ");
+	RunTest(SRC("arrays/arrays14.arco"), "1 2 3 4 5 6 7 8 9 10 11 99 ");
+	RunTest(SRC("arrays/decay_testing.arco"), "9 14 235 11 5 77 22 76 11 9");
+	
+	RunTest(SRC("structs/structs1.arco"), "55 44");
+	RunTest(SRC("structs/structs2.arco"), "55 44");
+	RunTest(SRC("structs/structs3.arco"), "55 44");
+	RunTest(SRC("structs/structs4.arco"), "55 44");
+	RunTest(SRC("structs/structs5.arco"), "124 66 25");
+	RunTest(SRC("structs/structs6.arco"), "124 66 25");
+	RunTest(SRC("structs/structs7.arco"), "55 99");
+	RunTest(SRC("structs/structs8.arco"), "55 55 55");
+	
+	RunTest(SRC("returns/returns1.arco"), "55 55");
+	RunTest(SRC("returns/returns2.arco"), "55 55");
+	RunTest(SRC("returns/returns3.arco"), "55 55");
+	RunTest(SRC("returns/returns4.arco"), "55 55");
+	RunTest(SRC("returns/returns5.arco"), "55 55");
+	RunTest(SRC("returns/returns6.arco"), "55 55");
+	
+	RunTest(SRC("member_functions/member_functions1.arco"), "1234");
+	RunTest(SRC("member_functions/member_functions2.arco"), "1234");
+	RunTest(SRC("member_functions/member_functions3.arco"), "foo1foo2");
+	RunTest(SRC("member_functions/member_functions4.arco"), "22");
+	RunTest(SRC("member_functions/constructors1.arco"), "53");
+	RunTest(SRC("member_functions/constructors2.arco"), "55");
+	
+	RunTest(SRC("globals/globals1.arco"), "44");
+	RunTest(SRC("globals/globals2.arco"), "32 186");
+	RunTest(SRC("globals/globals3.arco"), "34 66");
+	RunTest(SRC("globals/globals4.arco"), "22 1111");
+	RunTest(SRC("globals/globals5.arco"), "235 31 235 31 235 31 235 31 235 31 ");
+	
+	//RunTest(SRC("workpad"), "");
 	//RunTest(SRC("lots_of_errors.arco"), "");
 
 	if (Succeeded + Failed > 0) {
